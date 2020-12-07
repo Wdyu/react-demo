c045a766f4825951c47d354cbe1d8c603fda5da9
wudy <2415076963@qq.com>
2020-12-07 18:11:31 +0800

    新增

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react'
 import './assets/css/style.css'
 import Img from './assets/images/am7.jpg'
 
+let name = '小明',
+  age = 10,
+  list = [10, 20, 30, 40]
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        <p style={{ color: 'red', fontSize: 30 }}>JSX语法注意事项</p>
-        <p className='box'>JSX语法注意事项</p>
-        <img src={Img} width='200' />
+        <p>姓名：{name}</p>
+        <p>年龄：{age}</p>
+        <ul>
+          {list.map((el, key) => {
+            return <li key={key}>{el}</li>
+          })}
+        </ul>
+        <br />
+        <img src={Img} width='200' alt='el' />
         <br />
         <label htmlFor='username'>
           用户名：
